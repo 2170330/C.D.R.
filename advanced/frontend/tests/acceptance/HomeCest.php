bd853aadc91afdf76f43afc7dcc7a551299574ee
Dinis Roxo <2170353@my.ipleiria.pt>
2019-01-22 20:05:26 +0000

    teste aceitação

--- advanced/frontend/tests/acceptance/HomeCest.php
+++ advanced/frontend/tests/acceptance/HomeCest.php
@@ -11,7 +11,7 @@ class HomeCest
         $I->amOnPage(Url::toRoute('/site/index'));
         $I->see('C.D.R.');
 
-        // Página Login
+        /*// Página Login
         $I->click('LOGIN');
         //$I->wait(2); // wait for page to be opened
 
@@ -39,31 +39,113 @@ class HomeCest
         // Página Home
         $I->see('C.D.R.');
         $I->click('Logout');
-        //$I->wait(2);
+       */ //$I->wait(2);
         $I->click('LOGIN');
         //$I->wait(2);
 
         // Página Login
         $I->see('Iniciar sessão');
-        $I->wait(2);
+       // $I->wait(2);
         $I->fillField('Username', 'asd');
-        $I->wait(2);
+       // $I->wait(2);
         $I->fillField('Password', 123456);
-        $I->wait(2);
+       // $I->wait(2);
         $I->click('Login');
 
         // Página Home Logado
         $I->see('C.D.R.');
-        $I::
-        $I->submitForm('reservasForm', ['data' => '2019-01-25 00:30', 'nPessoas' => '2', 'id_user' => ]);
-       /* $I->selectOption('#reserva-npessoas', '2');
+
+        $I->amOnPage('index');
         $I->wait(2);
+        $I->see('Reservas', '.descricao');
+        $I->see('reservar', '.btn-reservar');
+        $I->see('1 pessoa', '#reserva-npessoas');
+
+        //Reservar mesa
+        $I->selectOption('Reserva[nPessoas]', '2');
+        $I->wait(2);
+
         $I->fillField('Reserva[data]', '2019-01-23 12:30');
+
+        $I->click('Reservar');
         $I->wait(2);
 
-        $I->click('Reservar');*/
 
+        $I->click('Voltar');
         $I->wait(2);
+
+        $I->click('MENU');
+        $I->wait(2);
+
+        // Página Menu
+            // Menus
+        $I->see('Menus');
+        $I->click('Menu');
+        $I->wait(2);
+            // Prato Carne
+        $I->moveMouseOver('.sidebar-menuPratos');
+        $I->wait(2);
+        $I->see('Carne');
+        $I->wait(2);
+        $I->click('Carne');
+        $I->wait(2);
+            // Prato Peixe
+        $I->moveMouseOver('.sidebar-menuPratos');
+        $I->wait(2);
+        $I->see('Peixe');
+        $I->wait(2);
+        $I->click('Peixe');
+        $I->wait(2);
+            // Prato Vegetariano
+        $I->moveMouseOver('.sidebar-menuPratos');
+        $I->wait(2);
+        $I->see('Vegetariano');
+        $I->wait(2);
+        $I->click('Vegetariano');
+        $I->wait(2);
+            // Prato Vegan
+        $I->moveMouseOver('.sidebar-menuPratos');
+        $I->wait(2);
+        $I->see('Vegan');
+        $I->wait(2);
+        $I->click('Vegan');
+        $I->wait(2);
+            // Bebidas Sumos
+        $I->moveMouseOver('.sidebar-menuBebidas');
+        $I->wait(2);
+        $I->moveMouseOver('.sidebar-menuBebidas');
+        $I->wait(2);
+        $I->see('Sumos');
+        $I->wait(2);
+        $I->click('Sumos');
+        $I->wait(2);
+            // Bebidas Vinhos
+        $I->moveMouseOver('.sidebar-menuBebidas');
+        $I->wait(2);
+        $I->see('Vinhos');
+        $I->wait(2);
+        $I->click('Vinhos');
+        $I->wait(2);
+            // Bebidas Outras Bebidas
+        $I->moveMouseOver('.sidebar-menuBebidas');
+        $I->wait(2);
+        $I->see('Outras Bebidas');
+        $I->wait(2);
+        $I->click('Outras Bebidas');
+        $I->wait(2);
+
+        $I->moveMouseOver('.sidebar-menu');
+        $I->wait(1);
+
+        $I->moveMouseOver('.sidebar-menu');
+        $I->wait(1);
+
+        $I->see('Sobremesas');
+
+        $I->click('Sobremesas');
+        $I->wait(2);
+
+
 
     }
 }
